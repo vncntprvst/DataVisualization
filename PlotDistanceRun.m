0d38256aeb1500714c41020aa1f605c5c2099744
Vincent Prevosto <vincent.prevosto@duke.edu>
2019-08-23 18:29:14 -0400

    simple distance plot

---
+++ PlotDistanceRun.m
@@ -0,0 +1,24 @@
+% Import csv file
+[fileName,pathName] = uigetfile('*.csv','Select CSV file');
+delimiter = ',';
+startRow = 2;
+
+formatSpec = '%f%f%[^\n\r]';
+fileID = fopen(fullfile(pathName,fileName),'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'TextType', 'string', 'EmptyValue', NaN, 'HeaderLines' ,startRow-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+fclose(fileID);
+
+runData = table(dataArray{1:end-1}, 'VariableNames', {'Value','TimestampTimeOfDayTotalMilliseconds'});
+clearvars filename delimiter startRow formatSpec fileID dataArray ans;
+
+% Adjust values
+runData.Value=runData.Value-runData.Value(1);
+runData.TimestampTimeOfDayTotalMilliseconds=runData.TimestampTimeOfDayTotalMilliseconds-runData.TimestampTimeOfDayTotalMilliseconds(1);
+
+%Plot
+figure;
+plot(runData.TimestampTimeOfDayTotalMilliseconds/60000,runData.Value*0.03,...
+    'k','Linewidth',2); %3cm off wheel center 
+title({'vIRt41 0813 training';'Distance run'})
+xlabel('Time (mn)')
+ylabel('Distance (m)')
